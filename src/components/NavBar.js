f74ad23612a527e64c4918bd7bf155093e859c83
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-28 18:22:44 -0400

    commit tree b4a643fafd51be6d9a87a987546a1318847b38e5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
-}
-
+  return <div>
+  <NavLink to="/">Home</NavLink>
+  <NavLink to="/Movies">Movies</NavLink>
+  <NavLink to="/Directors">Directors</NavLink>
+  <NavLink to="/Actors">Actors</NavLink>
+  </div>;
+  }
+  
 export default NavBar;
